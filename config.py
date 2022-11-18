64dbd03c61003ad58e2382158d66d9bb77a310fd
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-18 13:06:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+from os import getenv as e
+
+class API:
+    API_ID = e("API_ID", None)
+    API_HASH = e("API_HASH", None)
+
+class TOKENS:
+    BOT_TOKEN = e("BOT_TOKEN", None)
+
+class DATABASE:
+    MONGO_DB_URL = e("MONGO_DB_URL", None)
+
+class STUFF:
+    START_IMG = e("START_PIC", None)
