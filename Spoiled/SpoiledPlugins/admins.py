026ac8fe151e560494ea50a55ce8689e08333ffc
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-20 11:08:37 +0530

    Update admins.py

--- Spoiled/SpoiledPlugins/admins.py
+++ Spoiled/SpoiledPlugins/admins.py
@@ -1,5 +1,7 @@
 from config import DEV
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command(
 async def ban_unban(_, m):
     user_id = m.from_user.id
     chat_id = m.chat.id
