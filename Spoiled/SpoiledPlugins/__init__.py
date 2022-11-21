1fe86cf49229a5471777441f25252f73bc1256f8
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-21 17:47:02 +0530

    Create __init__.py

---
+++ Spoiled/SpoiledPlugins/__init__.py
@@ -0,0 +1,12 @@
+async def get_id(_, m):
+    try:
+        if m.reply_to_message:
+            id = m.reply_to_message.from_user.id
+        else:
+            txt = m.text.split()
+            if txt[1][0] == "@":
+                id = (await _.get_users(txt[1])).id
+            else:
+                id = int(txt[1])
+    except:
+        pass
