b871a55874d57fb723762778ccbb0fef3aed970e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-21 17:36:44 +0530

    Update admins.py

--- Spoiled/SpoiledPlugins/admins.py
+++ Spoiled/SpoiledPlugins/admins.py
@@ -13,7 +13,20 @@ async def ban_user(_, m):
             return False, "Can't ban chat owner !"
         if y.status == "administrator":
             return False, "Can't ban admin !"
+        if y.status == "banned":
+            return False, "User already banned !"
         await _.ban_user(m.chat.id, x)
+        return True, False
+    except Exception as e:
+        return False, e
+
+async def unban_user(_, m):
+    try:
+        x = await get_id(_, m)
+        y = await _.get_chat_member(m.chat.id, x)
+        if y.status != "banned":
+            return False, "User is not banned !"
+        await _.unban_user(m.chat.id, x)
         return True, False
     except Exception as e:
         return False, e
@@ -30,12 +43,22 @@ async def ban_unban(_, m):
                 return await m.reply("you need to be admin to do this !")
             if not x.can_restrict_members:
                 return await m.reply("you don't have to right to perform this action !")
+            if m.text.split(0)[1].lower() == "u":
+                unbanned, optional = await unban_user(_, m)
+                if not unbanned:
+                    return await m.reply(optional)
+                return await m.reply("User unbanned !")
             banned, optional = await ban_user(_, m)
             if not banned:
                 return await m.reply(optional)
             men = (await _.get_users(await get_id(_, m))).mention
             return await m.reply(f"{men} banned !")
         else:
+            if m.text.split(0)[1].lower() == "u":
+                unbanned, optional = await unban_user(_, m)
+                if not unbanned:
+                    return await m.reply(optional)
+                return await m.reply("User unbanned !")
             banned, optional = await ban_user(_, m)
             if not banned:
                 return await m.reply(optional)
