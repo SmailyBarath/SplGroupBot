87720e3c6c81e1b062ec74a47f47b93214bdf6c0
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-22 10:15:21 +0530

    Update kang.py

--- Spoiled/SpoiledPlugins/kang.py
+++ Spoiled/SpoiledPlugins/kang.py
@@ -41,17 +41,23 @@ async def kang(u: Update, c: CallbackContext):
                     if format == "video" or format == "animated":
                         if stics == 50:
                             pack += 1
+                            if format == "video":
+                                await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, webm_sticker=open(x, "rb"))
+                            elif format == "animated":
+                                await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, tgs_sticker=open(x, "rb"))
+                            else:
+                                await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, png_sticker=open(x, "rb") if png else sticid)
                             break
                     else:
                         if stics == 120:
                             pack += 1
+                            if format == "video":
+                                await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, webm_sticker=open(x, "rb"))
+                            elif format == "animated":
+                                await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, tgs_sticker=open(x, "rb"))
+                            else:
+                                await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, png_sticker=open(x, "rb") if png else sticid)
                             break
-                if format == "video":
-                    await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, webm_sticker=open(x, "rb"))
-                elif format == "animated":
-                    await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, tgs_sticker=open(x, "rb"))
-                else:
-                    await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, png_sticker=open(x, "rb") if png else sticid)
             except:
                 pack = 1
                 if format == "video":
