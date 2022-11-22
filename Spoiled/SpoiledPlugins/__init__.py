911cb1b589c641e688554edffe363ef1d7d674df
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-22 09:19:52 +0530

    Update __init__.py

--- Spoiled/SpoiledPlugins/__init__.py
+++ Spoiled/SpoiledPlugins/__init__.py
@@ -1,5 +1,5 @@
 from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
-from config import SUPPORT_CHAT
+from config.CHATS import SUPPORT_CHAT
 from telegram.ext import ApplicationBuilder
 from config.TOKENS import BOT_TOKEN
 
