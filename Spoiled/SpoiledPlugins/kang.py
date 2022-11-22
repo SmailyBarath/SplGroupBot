22aa5c7fe4946c8f00484b8ab00b2abbbc60c679
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-22 09:57:53 +0530

    Update kang.py

--- Spoiled/SpoiledPlugins/kang.py
+++ Spoiled/SpoiledPlugins/kang.py
@@ -45,5 +45,11 @@ async def kang(u: Update, c: CallbackContext):
                             pack += 1
             except:
                 pack = 1
+                if format == "video":
+                    await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, webm_sticker=open(x, "rb"))
+                elif format == "animated":
+                    await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, tgs_sticker=open(x, "rb"))
+                else:
+                    await c.bot.create_new_sticker_set(user_id=user.id, name=name, title=title, emojis=emoji, png_sticker=open(x, "rb") if png else sticid)
                 
             
