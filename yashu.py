976f4c3329048b3d7fcde6a45d5b4e3ddc5ed8f5
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-24 14:38:55 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,10 +1,9 @@
 from pyrogram import Client, idle
 from config import API, TOKENS
 
-spoil = Client(
-               ":SPOILED-BOT:",
+spoil = Client(":SPOILED-BOT:",
                api_id=API.API_ID,
                api_hash=API.API_HASH,
-               bot_token=TOKENS.BOT_TOKEN
+               bot_token=TOKENS.BOT_TOKEN,
                plugins=dict(root="Spoiled/SpoiledPlugins")
                )
