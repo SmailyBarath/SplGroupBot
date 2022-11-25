0e7752a218025b744326f05197f18e78bf1503d6
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-25 08:01:55 +0530

    Create ud.py

---
+++ Spoiled/SpoiledPlugins/ud.py
@@ -0,0 +1,20 @@
+import requests
+from Spoiled import Yashu
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.constants import ParseMode
+
+
+def ud(update: Update, context: CallbackContext):
+    message = update.effective_message
+    text = message.text[len("/ud ") :]
+    results = requests.get(
+        f"https://api.urbandictionary.com/v0/define?term={text}"
+    ).json()
+    try:
+        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+    except:
+        reply_text = "No results found."
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+
+Yashu.add_handler(CommandHandler(["ud", "urban"], ud))
