2455b2d92322f11f663ac39c7ba14cfbc99547d7
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-25 16:39:33 +0530

    Update shippering.py

--- Spoiled/SpoiledPlugins/shippering.py
+++ Spoiled/SpoiledPlugins/shippering.py
@@ -100,7 +100,7 @@ async def couple(_, message):
 
 {c1_mention} + {c2_mention} = ❤️
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
-            await app.send_message(message.chat.id, text=couple_selection_message)
+            await _.send_message(message.chat.id, text=couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
             await save_couple(chat_id, today, couple)
 
