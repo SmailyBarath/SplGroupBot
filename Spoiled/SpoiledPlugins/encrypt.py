e861b77ba64d992f344e76a98a099eda5253f556
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-25 16:16:52 +0530

    Create encrypt.py

---
+++ Spoiled/SpoiledPlugins/encrypt.py
@@ -0,0 +1,34 @@
+import secureme
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("encrypt"))
+async def encrypt(_, m):
+    if m.reply_to_message:
+        if m.reply_to_message.text:
+            txt = m.reply_to_message.text
+        else:
+            return
+    else:
+        if len(m.command) > 1:
+            txt = m.text.split(None, 1)[1]
+        else:
+            return
+    if txt:
+        k = secureme.encrypt(txt)
+        await m.reply(k)
+
+@Client.on_message(filters.command("decrypt"))
+async def decrypt(_, m):
+    if m.reply_to_message:
+        if m.reply_to_message.text:
+            txt = m.reply_to_message.text
+        else:
+            return
+    else:
+        if len(m.command) > 1:
+            txt = m.text.split(None, 1)[1]
+        else:
+            return
+    if txt:
+        k = secureme.decrypt(txt)
+        await m.reply(k)
