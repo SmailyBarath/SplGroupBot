b14cb403a4768b68bd5c5fc62ad7bf65703cccdc
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-26 00:21:48 +0530

    Create wish.py

---
+++ Spoiled/SpoiledPlugins/wish.py
@@ -0,0 +1,21 @@
+import random
+from pyrogram import filters, Client as pbot
+
+@pbot.on_message(filters.command("wish"))
+async def wish(_, m):
+    try:
+        txt = m.text.split(None, 1)[1]
+    except:
+        return await m.reply("""You can use /wish as a general Wishing Well of sorts
+
+For example:
+/wish I could date you 😍, or
+/wish that sushi was 🍣 in...""")
+
+    x = []
+    for i in range(0, 101):
+        x.append(i)
+    y = random.choice(x)
+    return await m.reply(f"""Your wish has been cast. ✨
+
+chance of success: {y}%""")
