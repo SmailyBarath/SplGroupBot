455956e8cf4bda82b3fb01b4edae71eefbc948a0
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-25 20:15:56 +0530

    Update helpers.py

--- Spoiled/SpoiledPlugins/helpers.py
+++ Spoiled/SpoiledPlugins/helpers.py
@@ -1,1 +1,54 @@
+from functools import wraps
+from telegram import Update
+from telegram.ext import CallbackContext
+from config import DEV
 
+DEV_USERS = [OWNER_ID] + SUDO_USERS
+
+async def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
+    if (
+        chat.type == "private"
+        or user_id in DEV_USERS
+        or chat.all_members_are_administrators
+        or user_id in [777000, 5754821527]
+    ):
+        return True
+    if not member:
+        with THREAD_LOCK:
+            # try to fetch from cache first.
+            try:
+                return user_id in ADMIN_CACHE[chat.id]
+            except KeyError:
+                # keyerror happend means cache is deleted,
+                # so query bot api again and return user status
+                # while saving it in cache for future useage...
+                chat_admins = await dispatcher.bot.getChatAdministrators(chat.id)
+                admin_list = [x.user.id for x in chat_admins]
+                ADMIN_CACHE[chat.id] = admin_list
+
+                return user_id in admin_list
+    else:
+        return member.status in ("administrator", "creator")
+
+def user_admin(func):
+    @wraps(func)
+    def is_admin(update: Update, context: CallbackContext, *args, **kwargs):
+        bot = context.bot
+        user = update.effective_user
+        chat = update.effective_chat
+
+        if user and is_user_admin(chat, user.id):
+            return func(update, context, *args, **kwargs)
+        if not user:
+            pass
+        elif DEL_CMDS and " " not in update.effective_message.text:
+            try:
+                update.effective_message.delete()
+            except:
+                pass
+        else:
+            update.effective_message.reply_text(
+                "Who dis non-admin telling me what to do? You want a punch?",
+            )
+
+    return is_admin
