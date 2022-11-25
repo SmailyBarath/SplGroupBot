d1f83c0c874f026d6733acae8099c9dcdd599a80
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-25 08:03:17 +0530

    Update ud.py

--- Spoiled/SpoiledPlugins/ud.py
+++ Spoiled/SpoiledPlugins/ud.py
@@ -5,7 +5,7 @@ from telegram.ext import CallbackContext
 from telegram.constants import ParseMode
 
 
-def ud(update: Update, context: CallbackContext):
+async def ud(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text[len("/ud ") :]
     results = requests.get(
@@ -15,6 +15,6 @@ def ud(update: Update, context: CallbackContext):
         reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
     except:
         reply_text = "No results found."
-    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+    await message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 Yashu.add_handler(CommandHandler(["ud", "urban"], ud))
