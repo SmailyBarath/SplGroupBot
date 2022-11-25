a6a87581f275c9abf90962a2d6975c5cf74c0cb5
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-26 00:32:44 +0530

    Create webshot.py

---
+++ Spoiled/SpoiledPlugins/webshot.py
@@ -0,0 +1,23 @@
+from pyrogram import filters, Client as app
+from pyrogram.types import Message
+
+@app.on_message(filters.command("webss"))
+async def take_ss(_, message: Message):
+    try:
+        if len(message.command) != 2:
+            return await message.reply_text(
+                "Give A Url To Fetch Screenshot."
+            )
+        url = message.text.split(None, 1)[1]
+        m = await message.reply_text("**Taking Screenshot**")
+        await m.edit("**Uploading**")
+        try:
+            await message.reply_photo(
+                photo=f"https://webshot.amanoteam.com/print?q={url}",
+                quote=False,
+            )
+        except:
+            return await m.edit("No Such Website.")
+        await m.delete()
+    except Exception as e:
+        await message.reply_text(str(e))
