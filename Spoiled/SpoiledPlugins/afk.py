7c0e54296542211fda3e3b0f72755133a1a9d2b8
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-26 16:02:39 +0530

    Update afk.py

--- Spoiled/SpoiledPlugins/afk.py
+++ Spoiled/SpoiledPlugins/afk.py
@@ -1,146 +1,39 @@
-import random, html
-from Spoiled import Yashu
-from Spoiled.SqLDatabase import afk_sql as sql
-from pyrogram import Client
-from pyrogram.types import Message
-from telegram import MessageEntity, Update
-from telegram.error import BadRequest
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import (
-    CallbackContext,
-    CallbackQueryHandler,
-    CommandHandler,
-    filters as Filters,
-    MessageHandler
-)
-from telegram.constants import ParseMode
-AFK_GROUP = 7
-AFK_REPLY_GROUP = 8
+from pyrogram import Client, filters
+from Spoiled.Database.afk import *
+import time
 
-async def get_user_id(username):
-    x = (await Client.get_users(username)).id
-    return x
+@Client.on_message(filters.command("afk") | filters.command("brb", ""))
+async def afk(_, m):
+    user_id = m.from_user.id
+    first_name = m.from_user.first_name
+    await m.reply(f"**{first_name}** is not AFK")
+    reason = m.text.split(None, 1)[1] if len(m.command) > 1 else None
+    start = time.time()
+    details = {"reason": reason, "time": start}
+    await add_afk(user_id, details)
 
-async def afk(update: Update, context: CallbackContext):
-    args = update.effective_message.text.split(None, 1)
-    user = update.effective_user
+B_UN = None
 
-    if not user:  # ignore channels
+@Client.on_message(group=1):
+async def cwf(_, m):
+    global B_UN
+    if not B_UN:
+        B_UN = (await _.get_me()).username
+    user_id = m.from_user.id
+    if m.text:
+        if m.text.split()[0] in ["/afk", f"/afk@{B_UN}", "brb"]:
+            return
+    afk, details = await is_afk(user_id)
+    if not afk:
         return
-
-    if user.id in [777000, 1087968824]:
-        return
-
-    notice = ""
-    if len(args) >= 2:
-        reason = args[1]
-        if len(reason) > 100:
-            reason = reason[:100]
-            notice = "\nYour afk reason was shortened to 100 characters."
+    first_name = m.from_user.first_name
+    start = details["time"]
+    reason = details["reason"]
+    end = time.time()
+    dur = end - start
+    dur = get_readable_time(int(dur))
+    if reason:
+        await m.reply(f"**{first_name}** is back online and was away for {dur}.\n\n**Reason** : `{reason}`)
     else:
-        reason = ""
-
-    sql.set_afk(update.effective_user.id, reason)
-    fname = update.effective_user.first_name
-    try:
-        await update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
-    except BadRequest:
-        pass
-
-
-async def no_longer_afk(update: Update, context: CallbackContext):
-    user = update.effective_user
-    message = update.effective_message
-
-    if not user:  # ignore channels
-        return
-
-    res = sql.rm_afk(user.id)
-    if res:
-        if message.new_chat_members:  # dont say msg
-            return
-        firstname = update.effective_user.first_name
-        try:
-            options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
-            ]
-            chosen_option = random.choice(options)
-            await update.effective_message.reply_text(chosen_option.format(firstname))
-        except:
-            return
-
-
-async def reply_afk(update: Update, context: CallbackContext):
-    bot = context.bot
-    message = update.effective_message
-    userc = update.effective_user
-    userc_id = userc.id
-    if message.entities and message.parse_entities(
-        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
-    ):
-        entities = message.parse_entities(
-            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
-        )
-
-        chk_users = []
-        for ent in entities:
-            if ent.type == MessageEntity.TEXT_MENTION:
-                user_id = ent.user.id
-                fst_name = ent.user.first_name
-
-                if user_id in chk_users:
-                    return
-                chk_users.append(user_id)
-
-            if ent.type != MessageEntity.MENTION:
-                return
-
-            user_id = get_user_id(message.text[ent.offset : ent.offset + ent.length])
-            if not user_id:
-                # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
-                return
-
-            if user_id in chk_users:
-                return
-            chk_users.append(user_id)
-
-            try:
-                chat = bot.get_chat(user_id)
-            except BadRequest:
-                print("Error: Could not fetch userid {} for AFK module".format(user_id))
-                return
-            fst_name = chat.first_name
-
-            await check_afk(update, context, user_id, fst_name, userc_id)
-
-    elif message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        fst_name = message.reply_to_message.from_user.first_name
-        await check_afk(update, context, user_id, fst_name, userc_id)
-
-
-async def check_afk(update, context, user_id, fst_name, userc_id):
-    if sql.is_afk(user_id):
-        user = sql.check_afk_status(user_id)
-        if int(userc_id) == int(user_id):
-            return
-        if not user.reason:
-            res = "{} is afk".format(fst_name)
-            await update.effective_message.reply_text(res)
-        else:
-            res = "{} is afk.\nReason: <code>{}</code>".format(
-                html.escape(fst_name), html.escape(user.reason)
-            )
-            await update.effective_message.reply_text(res, parse_mode="html")
-
-Yashu.add_handler(CommandHandler("afk", afk))
-Yashu.add_handler(CommandHandler(Filters.Regex(r"^(?i)brb(.*)$"), afk))
-Yashu.add_handler(MessageHandler(Filters.ALL & Filters.chat_type.groups, no_longer_afk))
-Yashu.add_handler(MessageHandler(Filters.ALL & Filters.chat_type.groups, reply_afk))
+        await m.reply(f"**{first_name}** is back online and was away for {dur}.")
+    await del_afk(user_id)
