5abf1b51529b4bb3360dcbac9bb533d3bad007fd
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-26 19:34:16 +0530

    Update alive.py

--- Spoiled/SpoiledPlugins/alive.py
+++ Spoiled/SpoiledPlugins/alive.py
@@ -6,13 +6,13 @@ import time
 
 DEV_USERS = DEV.SUDO_USERS + [DEV.OWNER_ID]
 
-@Client.on_message(filters.command(["alive", "ping"]) & filters.user(DEV_USERS))
+@Client.on_message(filters.command(["alive", "ping"]))
 async def alive(_, m):
     start = time.time()
     ok = await m.reply("`Building Alive and Pinging..!`")
     myfn = (await _.get_me()).first_name
-    txt = f"**Hi {m.from_user.first_name} !, I'm {myfn}**\n"
-    txt += f"**Bot owner : [Yashu-Alpha](t.me/{DEV.OWNER_USERNAME})**\n"
+    txt = f"**Hi {m.from_user.first_name} !, I'm {myfn}**\n\n"
+    txt += f"**Bot owner : [Yashu-Alpha](t.me/{DEV.OWNER_USERNAME})**\n\n"
     if DEV.SUDO_USERS:
         txt += f"**Sudo status : ✅ , {len(DEV.SUDO_USERS)}**\n"
     else:
@@ -20,4 +20,4 @@ async def alive(_, m):
     end = time.time()
     dur = (str(end-start))[0:5]
     await ok.delete()
-    await m.reply_photo(STUFF.PING_IMG, caption=txt + "\n" + f"**Ping : {dur}s**")
+    await m.reply_photo(STUFF.PING_IMG, caption=txt + "\n" + f"**Ping : {dur}S**")
