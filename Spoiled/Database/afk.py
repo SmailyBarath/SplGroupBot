bc9a851fcaf37d00f6cd8b7491cb08bcab1d275b
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-26 15:44:15 +0530

    Create afk.py

---
+++ Spoiled/Database/afk.py
@@ -0,0 +1,24 @@
+from . import db
+
+afkdb = db.afk
+
+async def is_afk(user_id: int):
+    x = afkdb.find_one({"user_id": user_id})
+    if not x:
+        return False, {}
+    return True, x["details"]
+
+async def add_afk(user_id: int, details):
+    await afkdb.update_one({"user_id": user_id}, {"$set": {"details": details}}, upsert=True)
+
+async def del_afk(user_id: int):
+    await afkdb.delete_one({"user_id": user_id})
+
+async def get_afk_users():
+    x = afkdb.find({"user_id": {"$gt": 0}})
+    if not x:
+        return 0
+    l = []
+    for z in await x.to_list(length=1000000000):
+        l.append(x["user_id"])
+    return len(l)
