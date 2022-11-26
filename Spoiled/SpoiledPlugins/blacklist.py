e818f1c03d43831e67a8a635996e65f6d2e77fa4
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-26 22:31:33 +0530

    Update blacklist.py

--- Spoiled/SpoiledPlugins/blacklist.py
+++ Spoiled/SpoiledPlugins/blacklist.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from Spoiled.Database.blacklist import *
 from config import DEV
+from . import IKM, IKB
 
 DEV_USERS = [DEV.OWNER_ID] + DEV.SUDO_USERS
 
@@ -33,4 +34,24 @@ async def rmblacklist(_, m):
         return await m.reply(f"`{word}` isn't blacklisted !")
     await del_blacklist(m.chat.id, word)
     await m.reply(f"`{word}` removed from blacklist !")
+
+markup = IKM(
+         [
+         [
+         IKB("Clear all 🗑️", callback_data="clear_all")
+         ]
+         ]
+         )
     
+@Client.on_message(filters.command(["blacklist", "blocklist"]))
+async def gbl(_, m):
+    if not m.from_user.id in DEV_USERS:
+        x = await _.get_chat_member(m.chat.id, m.from_user.id)
+        if not x.status in ["creator", "administrator"]:
+            return await m.reply("Only admin can perform this action !")
+    li = await get_blacklist(m.chat.id)
+    txt = f"**Words blacklist :** {m.chat.title}"
+    txt += "\n\n"
+    for h in li:
+        txt += f"-`{h}`\n"
+    await m.reply(txt, reply_markup=markup)
