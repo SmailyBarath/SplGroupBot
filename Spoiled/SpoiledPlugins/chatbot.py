e83bd5427aceb2de820e5d1240dd685013b8b727
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 20:58:32 +0530

    Update chatbot.py

--- Spoiled/SpoiledPlugins/chatbot.py
+++ Spoiled/SpoiledPlugins/chatbot.py
@@ -62,7 +62,6 @@ async def type_and_send(message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id if message.from_user else 0
     query = message.text.strip()
-    await message._client.send_chat_action(chat_id, "typing")
     response, _ = await gather(lunaQuery(query, user_id), sleep(3))
     await message.reply_text(response)
 
