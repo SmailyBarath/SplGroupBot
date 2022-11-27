dd3fe79e5b357853a44eb7ad0150ff4bb76945f4
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 15:36:59 +0530

    Update sysinfo.py

--- Spoiled/SpoiledPlugins/sysinfo.py
+++ Spoiled/SpoiledPlugins/sysinfo.py
@@ -6,6 +6,7 @@ TOTAL_RAM = (p.virtual_memory().total / 10000000000)
 RAM_USEDP = p.virtual_memory().percent
 RAM_USED = str((RAM_USEDP * TOTAL_RAM) / 100)[0:4]
 TOTAL_RAM = str(p.virtual_memory().total / 10000000000)[0:4]
+DISK_USAGE = str(p.disk_usage("/").percent)
 IMG = "https://te.legra.ph/file/70ef0b7a6c8fece1a13c0.jpg"
 
 txt = "\n"
@@ -14,6 +15,8 @@ txt += "\n\n"
 txt += f"• **OS : Linux's Ubuntu**"
 txt += "\n\n"
 txt += f"**• CPU : {CPU}%**"
+txt += "\n\n"
+txt += f"**• DISK : {DISK_USAGE}**"
 txt += "\n\n"
 txt += f"**• MEMORY USED : {RAM_USED}GB ({RAM_USEDP}%)**"
 txt += "\n\n"
