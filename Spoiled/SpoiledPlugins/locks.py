f852a3642f5c4b5a91dba3b897e713d57c9fe524
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 13:07:39 +0530

    Update locks.py

--- Spoiled/SpoiledPlugins/locks.py
+++ Spoiled/SpoiledPlugins/locks.py
@@ -1,12 +1,17 @@
 from pyrogram import Client, filters
 from config import DEV
+from Spoiled.Database.lock import *
+from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 
 DEV_USERS = DEV.SUDO_USERS + [DEV.OWNER_ID]
 
-AVAILABLE = ["photo", "video", "doc", voice", "audio", "gif", "url"]
+AVAILABLE = ["photo", "video", "document", voice", "audio", "gif", "url"]
 
-@Client.on_message(filters.command("lock"))
+ASYNC = None
+
+@Client.on_message(filters.command("lock") & filters.group)
 async def lock(_, m):
+    global ASYNC
     if not m.from_user.id in DEV_USERS:
         x = await _.get_chat_member(m.chat.id, m.from_user.id)
         if not x.status in ["creator", "administrator"]:
@@ -21,9 +26,11 @@ async def lock(_, m):
         return await m.reply(f"**{locktype}s** already locked in {m.chat.title}")
     await add_lock(m.chat.id, locktype)
     await m.reply(f"**{locktype}s** are now locked in **{m.chat.title}**")
+    ASYNC = True
 
-@Client.on_message(filters.command("unlock"))
+@Client.on_message(filters.command("unlock") & filters.group)
 async def unlock(_, m):
+    global ASYNC
     if not m.from_user.id in DEV_USERS:
         x = await _.get_chat_member(m.chat.id, m.from_user.id)
         if not x.status in ["creator", "administrator"]:
@@ -38,6 +45,7 @@ async def unlock(_, m):
         return await m.reply(f"**{locktype}s** aren't locked in {m.chat.title}")
     await del_lock(m.chat.id, locktype)
     await m.reply(f"**{locktype}s** are now unlocked in **{m.chat.title}**")
+    ASYNC = True
 
 @Client.on_message(filters.command("locktypes"))
 async def locktypes(_, m):
@@ -50,3 +58,126 @@ async def locktypes(_, m):
     for z in AVAILABLE:
         txt += f"- {z}\n"
     await m.reply(txt)
+
+markup = IKM(
+         [
+         [
+         IKB("Clear 🗑️", callback_data="clear_locks")
+         ]
+         ]
+         )
+
+@Client.on_message(filters.command("locks") & filters.group)
+async def locks(_, m):
+    if not m.from_user.id in DEV_USERS:
+        x = await _.get_chat_member(m.chat.id, m.from_user.id)
+        if not x.status in ["creator", "administrator"]:
+            return await m.reply("Only admins are allowed to perform !")
+    d = await get_locks(m.chat.id)
+    if not d:
+        return await m.reply(f"Nothing locked in **{m.chat.title}**")
+    txt = f"**Locks in {m.chat.title}**"
+    txt += "\n\n"
+    for j in d:
+        txt += f"- {j}\n"
+    return await m.reply(txt, reply_markup=markup)
+
+@Client.on_callback_query(filters.regex("clear_locks"))
+async def lock_cbq(_, q):
+    global ASYNC
+    id = q.from_user.id
+    if not id in DEV_USERS:
+        x = await _.get_chat_member(q.message.chat.id, id)
+        if x.status != "creator":
+            return await q.answer("Only creator can clear all at once !", show_alert=True)
+    await q.answer("Clearing locks !")
+    await clear_locks(q.message.chat.id)
+    await q.edit_message_text("Cleared all locks !")
+    ASYNC = True
+
+LOCKS = []
+admins = []
+@Client.on_message(group=4)
+async def cwf(_, m):
+    global admins
+    if not admins:
+        async for z in _.get_chat_members(m.chat.id, filter="administrators"):
+            if not z.user.is_bot and not z.user.is_deleted:
+                admins.append(z.user.id)
+    if m.from_user.id in (DEV_USERS + admins):
+        return
+    global LOCKS
+    global ASYNC
+    if not LOCKS:
+        LOCKS = await get_locks(m.chat.id)
+    if ASYNC:
+        LOCKS = await get_locks(m.chat.id)
+        ASYNC = False
+    if not LOCKS:
+        return
+
+    if m.photo:
+        if "photo" in LOCKS:
+            try:
+                await m.delete()
+            except:
+                pass
+        return
+
+    if m.video:
+        if "video" in LOCKS:
+            try:
+                await m.delete()
+            except:
+                pass
+        return
+
+    if m.audio:
+        if "audio" in LOCKS:
+            try:
+                await m.delete()
+            except:
+                pass
+        return
+
+    if m.voice:
+        if "voice" in LOCKS:
+            try:
+                await m.delete()
+            except:
+                pass
+        return
+
+    if m.document:
+        if "document" in LOCKS:
+            try:
+                await m.delete()
+            except:
+                pass
+        return
+
+    if m.animation:
+        if "gif" in LOCKS:
+            try:
+                await m.delete()
+            except:
+                pass
+        return
+
+    if m.text or m.caption:
+        if "url" in LOCKS:
+            txt = m.text.split() if m.text else m.caption.split()
+            for z in txt:
+                for t in z:
+                    if t == ".":
+                        ind = z.index(".")
+                        if z[ind - 1] != " " and z[ind + 1] != " ":
+                            try:
+                                await m.delete()
+                            except:
+                                pass
+        return
+    
+    
+    
+    
