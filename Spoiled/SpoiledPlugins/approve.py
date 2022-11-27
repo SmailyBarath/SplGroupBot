c6d481308e82061beb81b7369f531cac7ee92e28
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 16:56:31 +0530

    Update approve.py

--- Spoiled/SpoiledPlugins/approve.py
+++ Spoiled/SpoiledPlugins/approve.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from Spoiled.Database.approve import *
 from . import get_id, verify
 from config import DEV
+from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 
 DEV_USERS = DEV.SUDO_USERS + [DEV.OWNER_ID]
 
@@ -37,3 +38,31 @@ async def disapprove_user(_, m):
         id = await get_id(_, m)
     except:
         return await m.reply("Atleast reply to user or provide id !")
+    lol = await is_approved(id)
+    fn = (await _.get_users(id)).first_name
+    if not lol:
+        return await m.reply(f"**{fn}** wasn't approved in **{m.chat.title}**")
+    await disapprove(m.chat.id, id)
+    await m.reply(f"**{fn}** disapproved in **{m.chat.title}** !")
+
+markup = IKM(
+         [
+         [
+         IKB("Disapprove all", callback_data="disapprove_all")
+         ]
+         ]
+         )
+
+@Client.on_message(filters.command("approved"))
+async def approved(_, m):
+    x, y = await verify(_, m)
+    if not x:
+        return await m.reply(y)
+    g = await get_approved(m.chat.id)
+    if not g:
+        return await m.reply(f"No approved users in **{m.chat.title}**")
+    txt = f"**Approved users in {m.chat.title}**"
+    txt += "\n\n"
+    for j in g:
+        txt += f"- `{j}`\n"
+    await m.reply(txt, reply_markup=markup)
