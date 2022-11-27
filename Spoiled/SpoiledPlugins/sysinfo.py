919917d6a9be324a971e44e41c3f9ed3598d832f
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 12:01:02 +0530

    Update sysinfo.py

--- Spoiled/SpoiledPlugins/sysinfo.py
+++ Spoiled/SpoiledPlugins/sysinfo.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 
 CPU = p.cpu_percent(3)
 RAM_USED = str(p.virtual_memory()[3] / 10000000000)[0:4]
-TOTAL_RAM = str(p.virtual_memory().total / 1000)[0:4]
+TOTAL_RAM = str(p.virtual_memory().total / 100000000)[0:4]
 RAM_USEDP = p.virtual_memory()[2]
 IMG = "https://te.legra.ph/file/70ef0b7a6c8fece1a13c0.jpg"
 
@@ -14,7 +14,7 @@ txt += f"• **OS : Linux's Ubuntu**"
 txt += "\n\n"
 txt += f"**• CPU : {CPU}%**"
 txt += "\n\n"
-txt += f"**• MEMORY USED : {RAM_USED}GB ( {RAM_USEDP}% )**"
+txt += f"**• MEMORY USED : {RAM_USED}GB ({RAM_USEDP}%)**"
 txt += "\n\n"
 txt += f"**• RAM : {TOTAL_RAM}GB**"
 
