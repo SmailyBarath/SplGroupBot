4d263dcaf03fcd747d211ce4acf32093d9ab2bfb
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 11:50:01 +0530

    Update sysinfo.py

--- Spoiled/SpoiledPlugins/sysinfo.py
+++ Spoiled/SpoiledPlugins/sysinfo.py
@@ -6,7 +6,8 @@ TOTAL_RAM = str(p.virtual_memory().total)[0:4]
 RAM_USEDP = p.virtual_memory()[2]
 IMG = "https://te.legra.ph/file/70ef0b7a6c8fece1a13c0.jpg"
 
-txt = f"**SYS NAME : Alpha's VPS**"
+txt = "\n"
+txt += f"**SYS NAME : Alpha's VPS**"
 txt += "\n\n"
 txt += f"**OS : Linux's Ubuntu**"
 txt += "\n\n"
