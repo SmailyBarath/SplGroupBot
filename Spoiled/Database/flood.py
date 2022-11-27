3e13400a76230f9f1130143e1b337f564452391f
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 23:06:55 +0530

    Update flood.py

--- Spoiled/Database/flood.py
+++ Spoiled/Database/flood.py
@@ -1,1 +1,23 @@
+from . import db
 
+flood_toggle_db = db.floods
+
+async def is_flood_on(chat_id: int) -> bool:
+    chat = await flood_toggle_db.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
+
+
+async def flood_on(chat_id: int):
+    is_flood = await is_flood_on(chat_id)
+    if is_flood:
+        return
+    return await flood_toggle_db.delete_one({"chat_id": chat_id})
+
+
+async def flood_off(chat_id: int):
+    is_flood = await is_flood_on(chat_id)
+    if not is_flood:
+        return
+    return await flood_toggle_db.insert_one({"chat_id": chat_id})
