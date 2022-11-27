2c2ad11bfce956d21302c3725573748ac0b07ecb
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 22:35:36 +0530

    Update dice.py

--- Spoiled/SpoiledPlugins/dice.py
+++ Spoiled/SpoiledPlugins/dice.py
@@ -2,4 +2,7 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("dice"))
 async def dice(_, m):
-    await _.send_dice(m.chat.id, "🎲")
+    try:
+        await _.send_dice(m.chat.id, "🎲")
+    except:
+        pass
