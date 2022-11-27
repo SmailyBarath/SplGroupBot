4a9c41f559cc1ddb582347fa31a4880229ac152d
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-27 11:54:58 +0530

    Update sysinfo.py

--- Spoiled/SpoiledPlugins/sysinfo.py
+++ Spoiled/SpoiledPlugins/sysinfo.py
@@ -2,19 +2,19 @@ import psutil as p
 from pyrogram import Client, filters
 
 CPU = p.cpu_percent(3)
-RAM_USED = str(p.virtual_memory()[3] / 1000000000)[0:4]
-TOTAL_RAM = str(p.virtual_memory().total)[0:4]
+RAM_USED = str(p.virtual_memory()[3] / 10000000000)[0:4]
+TOTAL_RAM = str(p.virtual_memory().total / 1000)[0:4]
 RAM_USEDP = p.virtual_memory()[2]
 IMG = "https://te.legra.ph/file/70ef0b7a6c8fece1a13c0.jpg"
 
 txt = "\n"
-txt += f"**SYS NAME : Alpha's VPS**"
+txt += f"• **SYS NAME : Alpha's VPS**"
 txt += "\n\n"
-txt += f"**OS : Linux's Ubuntu**"
+txt += f"• **OS : Linux's Ubuntu**"
 txt += "\n\n"
 txt += f"**• CPU : {CPU}%**"
 txt += "\n\n"
-txt += f"**• MEMORY USED : {RAM_USED}GB ( {RAM_USEDP} )**"
+txt += f"**• MEMORY USED : {RAM_USED}GB ( {RAM_USEDP}% )**"
 txt += "\n\n"
 txt += f"**• RAM : {TOTAL_RAM}GB**"
 
