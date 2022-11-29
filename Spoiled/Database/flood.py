dd20ba722f07b9815b4c743e71b2145d96b3cc94
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 11:59:50 +0530

    Update flood.py

--- Spoiled/Database/flood.py
+++ Spoiled/Database/flood.py
@@ -2,6 +2,29 @@ from . import db
 
 flood_toggle_db = db.floods
 
+fmdb = db.flood
+
+fdb = flood_toggle_db
+
+async def set_flood_value(chat_id: int, value):
+    await fdb.update_one({"chat_id", chat_id}, {"$set": {"value": value}}, upsert=True)
+
+async def set_flood_mode(chat_id: int, mode):
+    await fmdb.update_one({"chat_id", chat_id}, {"$set": {"value": value}}, upsert=True)
+
+async def get_flood(chat_id: int):
+    x = await fdb.find_one({"chat_id": chat_id})
+    if not x:
+        return 0
+    return x["value"]
+
+async def get_flood_mode(chat_id: int):
+    x = await fmdb.find_one({"chat_id": chat_id})
+    if not x:
+        return "delete"
+    return x["mode"]
+
+
 async def is_flood_on(chat_id: int) -> bool:
     chat = await flood_toggle_db.find_one({"chat_id": chat_id})
     if not chat:
