eb8e1df76068c9e9fe14da977cd1f46a68d2c574
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 10:26:34 +0530

    Update admins.py

--- Spoiled/SpoiledPlugins/admins.py
+++ Spoiled/SpoiledPlugins/admins.py
@@ -42,7 +42,7 @@ async def list_admin_rights(_, m):
             admin_rights = {chat_id: {x: {"can_promote_members": True if h.can_promote_members else False}}}
             admin_rights = {chat_id: {x: {"can_invite_users": True if h.can_invite_users else False}}}
             admin_rights = {chat_id: {x: {"can_pin_messages": True if h.can_pin_messages else False}}}
-            admin_rights = {chat_id: {x: {"can_manage_voice_chats": True if h.can_manage_voice_chats else False}}}
+            admin_rights = {chat_id: {x: {"can_manage_video_chats": True if h.can_manage_video_chats else False}}}
         admin_rights[chat_id]["updated"] = time.time()
         return admin_rights
     if (int(time.time() - admin_rights[chat_id]["updated"])) > 3600:
@@ -55,7 +55,7 @@ async def list_admin_rights(_, m):
             admin_rights = {chat_id: {x: {"can_promote_members": True if h.can_promote_members else False}}}
             admin_rights = {chat_id: {x: {"can_invite_users": True if h.can_invite_users else False}}}
             admin_rights = {chat_id: {x: {"can_pin_messages": True if h.can_pin_messages else False}}}
-            admin_rights = {chat_id: {x: {"can_manage_voice_chats": True if h.can_manage_voice_chats else False}}}
+            admin_rights = {chat_id: {x: {"can_manage_video_chats": True if h.can_manage_video_chats else False}}}
         admin_rights[chat_id]["updated"] = time.time()
         return admin_rights
     if RELOAD:
@@ -68,7 +68,7 @@ async def list_admin_rights(_, m):
             admin_rights = {chat_id: {x: {"can_promote_members": True if h.can_promote_members else False}}}
             admin_rights = {chat_id: {x: {"can_invite_users": True if h.can_invite_users else False}}}
             admin_rights = {chat_id: {x: {"can_pin_messages": True if h.can_pin_messages else False}}}
-            admin_rights = {chat_id: {x: {"can_manage_voice_chats": True if h.can_manage_voice_chats else False}}}
+            admin_rights = {chat_id: {x: {"can_manage_video_chats": True if h.can_manage_video_chats else False}}}
         admin_rights[chat_id]["updated"] = time.time()
         return admin_rights
 
