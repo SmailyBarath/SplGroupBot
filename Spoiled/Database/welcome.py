25bff3aeb474517425a87e3a5bea752a8b4f207c
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 23:36:23 +0530

    Update welcome.py

--- Spoiled/Database/welcome.py
+++ Spoiled/Database/welcome.py
@@ -2,6 +2,8 @@ from . import db
 
 welcomedb = db.welcome
 
+wdb = db.well
+
 async def set_welcome(chat_id: int, msg_id: int):
     await welcomedb.update_one({"chat_id": chat_id}, {"$set": {"msg_id": msg_id}}, upsert=True)
 
@@ -12,7 +14,13 @@ async def get_welcome(chat_id: int):
     return x["msg_id"]
 
 async def is_welcome_on(chat_id: int):
-    x = await welcomedb.find_one({"chat_id": chat_id})
+    x = await wdb.find_one({"chat_id": chat_id})
     if x:
         return True
     return False
+
+async def toggle_welcome(chat_id: int):
+    x = await wdb.find_one({"chat_id": chat_id})
+    if x:
+        return await wdb.delete_one({"chat_id": chat_id})
+    return await wdb.insert_one({"chat_id": chat_id})
