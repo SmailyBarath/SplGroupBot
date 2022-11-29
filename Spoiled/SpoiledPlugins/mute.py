45a32a7cf63701cd396677f2083347e2775ffe4a
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 17:58:24 +0530

    Update mute.py

--- Spoiled/SpoiledPlugins/mute.py
+++ Spoiled/SpoiledPlugins/mute.py
@@ -83,3 +83,61 @@ async def unban(_, m):
     if not g:
         return await m.reply(h)
     await m.reply(h)
+
+async def tmute_user(_, m):
+    try:
+        id = await get_id(_, m)
+    except:
+        return False, f"**Reply to a user or provide id !**"
+    if not m.reply_to_message:
+        if len(m.command) > 2:
+            try:
+                tim = int(m.text.split(None, 3)[2])
+            except:
+                return await m.reply(f"**Enter a value which will be considered in minutes !**")
+        if len(m.command) > 3: 
+            reason = m.text.split(None, 3)[3]      
+    else:
+        if len(m.command) > 1:
+            try:
+                tim = int(m.text.split(None, 2)[1])
+            except:
+                return await m.reply(f"**Enter a value which will be considered in minutes !**")
+        if len(m.command) > 2: 
+            reason = m.text.split(None, 2)[2]
+    myid = (await _.get_me()).id
+    if id == myid:
+        return False, "😒😒.."
+    if id in DEV_USERS:
+        return False, f"**Can't mute sudo users !**"
+    x = await _.get_chat_member(m.chat.id, id)
+    if x.privileges:
+        return False, f"**Can't mute an admin !**"
+    await _.restrict_chat_member(m.chat.id, id, permissions=ChatPermissions())
+    men = (await _.get_users(id)).mention
+    if reason:
+        return True, f"**{men} muted for {tim}min!\n\nReason : {reason}**"
+    else:
+        return True, f"**{men} muted for {tim}min!**"
+
+@Client.on_message(filters.command(["tmute"]) & filters.group)
+async def ban(_, m):
+    id = m.from_user.id
+    if not id in DEV_USERS:
+        x = await _.get_chat_member(m.chat.id, id)
+        if not x.privileges:
+            return await m.reply("You need to be admin to do this !")
+        priv = x.privileges
+        if not priv.can_restrict_members:
+            return await m.reply("You got no rights to restrict !")
+    myid = (await _.get_me()).id
+    x = await _.get_chat_member(m.chat.id, myid)
+    x = x.privileges
+    if not x.can_restrict_members:
+        return await m.reply("I got no rights to restrict members !")
+    g, h = await tmute_user(_, m)
+    if not g:
+        return await m.reply(h)
+    if m.text.split()[0][1].lower() == "s":
+        return
+    await m.reply(h)
