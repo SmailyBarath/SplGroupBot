2d212a7346a94433a149600da8c7f6bd965b5817
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 11:03:03 +0530

    Update del_pin.py

--- Spoiled/SpoiledPlugins/del_pin.py
+++ Spoiled/SpoiledPlugins/del_pin.py
@@ -26,3 +26,22 @@ async def del(_, m):
         await m.delete()
     except:
         return
+
+@Client.on_message(filters.command(["pin", "unpin"]))
+async def pin(_, m):
+    id = m.from_user.id
+    g, h = await verify(_, m)
+    if not g:
+        return await m.reply(h)
+    g = await verify_right(m.chat.id, id, "can_pin_messages")
+    if not g:
+        return await m.reply("You got no right to do this")
+    if not m.reply_to_message:
+        return await m.reply("What I've to do ?")
+    if m.text.split()[0][1] == "u":
+        await m.reply_to_message.unpin()
+        await m.reply("Message unpinned !")
+    else:
+        await m.reply_to_message.pin()
+        await m.reply("Message pinned !")
+    
