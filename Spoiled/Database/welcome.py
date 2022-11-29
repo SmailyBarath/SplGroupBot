089d1bebc54d378badaf7e20808e74dabea8bc4c
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 22:14:43 +0530

    Update welcome.py

--- Spoiled/Database/welcome.py
+++ Spoiled/Database/welcome.py
@@ -4,3 +4,11 @@ welcomedb = db.welcome
 
 async def set_welcome(chat_id: int, msg_id: int):
     await welcomedb.update_one({"chat_id": chat_id}, {"$set": {"msg_id": msg_id}}, upsert=True)
+
+async def get_welcome(chat_id: int):
+    x = await welcomedb.find_one({"chat_id": chat_id})
+    if not x:
+        return None
+    return x["msg_id"]
+
+async def is_welcome_on(
