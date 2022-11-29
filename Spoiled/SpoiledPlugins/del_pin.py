425104e3da023eea01a983136182fc21efc86087
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 10:48:49 +0530

    Update del_pin.py

--- Spoiled/SpoiledPlugins/del_pin.py
+++ Spoiled/SpoiledPlugins/del_pin.py
@@ -9,5 +9,20 @@ async def del(_, m):
         try:
             return await m.delete()
         except:
-            pass
-    
+            return
+    g = await verify_right(m.chat.id, id, "can_delete_messages")
+    if not g:
+        try:
+            return await m.delete()
+        except:
+            return
+    if not m.reply_to_message:
+        try:
+            return await m.delete()
+        except:
+            return
+    try:
+        await m.reply_to_message.delete()
+        await m.delete()
+    except:
+        return
