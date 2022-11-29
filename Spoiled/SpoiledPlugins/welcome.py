449c74cbfb18b2942e9b64311635f9eeb5a6c179
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 22:01:24 +0530

    Update welcome.py

--- Spoiled/SpoiledPlugins/welcome.py
+++ Spoiled/SpoiledPlugins/welcome.py
@@ -1,3 +1,12 @@
 from pyrogram import Client, filters
+from config import CHATS
+from config import DEV
+
+DEV_USERS = DEV.SUDO_USERS + [DEV.OWNER_ID]
+
+DUMP = CHATS.MESSAGE_DUMP_CHAT
 
 @Client.on_message(filters.command("setwelcome") & filters.group)
+async def welcome_setter(_, m):
+ 
+    
