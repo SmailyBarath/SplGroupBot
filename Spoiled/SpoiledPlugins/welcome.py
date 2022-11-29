ac41a3886699f1985a10a6b1fd05da7f64595b1c
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-30 00:41:22 +0530

    Update welcome.py

--- Spoiled/SpoiledPlugins/welcome.py
+++ Spoiled/SpoiledPlugins/welcome.py
@@ -21,6 +21,14 @@ VALID_WELCOME_FORMATTERS = [
 
 @Client.on_message(filters.command("clearwelcome") & filters.group)
 async def welcomclr(_, m):
+    id = m.from_user.id
+    if not id in DEV_USERS:
+        x = await _.get_chat_member(m.chat.id, id)
+        if not x.privileges:
+            return
+        if not x.privileges.can_change_info:
+            return await m.reply(f"**You can't change welcome settings !**")
+    await clear_welcome(m.chat.id)
 
 @Client.on_message(filters.command("setwelcome") & filters.group)
 async def welcome_setter(_, m):
