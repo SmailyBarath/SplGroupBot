4b34d3377bc03c955fe4643905bbe7880c2e8681
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-30 00:35:23 +0530

    Update welcome.py

--- Spoiled/SpoiledPlugins/welcome.py
+++ Spoiled/SpoiledPlugins/welcome.py
@@ -19,6 +19,9 @@ VALID_WELCOME_FORMATTERS = [
     "mention",
 ]
 
+@Client.on_message(filters.command("clearwelcome") & filters.group)
+async def welcomclr(_, m):
+
 @Client.on_message(filters.command("setwelcome") & filters.group)
 async def welcome_setter(_, m):
     id = m.from_user.id
