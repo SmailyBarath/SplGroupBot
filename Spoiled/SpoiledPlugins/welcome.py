99f54d8f8dc510793c6c506472c057d9bee9a3d2
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-29 22:05:25 +0530

    Update welcome.py

--- Spoiled/SpoiledPlugins/welcome.py
+++ Spoiled/SpoiledPlugins/welcome.py
@@ -8,5 +8,12 @@ DUMP = CHATS.MESSAGE_DUMP_CHAT
 
 @Client.on_message(filters.command("setwelcome") & filters.group)
 async def welcome_setter(_, m):
- 
-    
+    id = m.from_user.id
+    if not id in DEV_USERS:
+        x = await _.get_chat_member(m.chat.id, id)
+        if not x.privileges:
+            return
+        if not x.privileges.can_change_info:
+            return await m.reply(f"**You can't change welcome settings !**")
+    if not m.reply_to_message:
+        return await m.reply(
