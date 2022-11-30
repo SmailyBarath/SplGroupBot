92a49b7d8153e9188de063ea060320608deb7bd3
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-30 16:48:24 +0530

    Update filters.py

--- Spoiled/Database/filters.py
+++ Spoiled/Database/filters.py
@@ -3,7 +3,13 @@ from . import db
 filtersdb = db.filters
 
 async def add_filter(chat_id: int, data):
-    await filterdb.update_one({"chat_id": chat_id}, {"$set": {"data": data}}, upsert=True)
+    x = await filterdb.find_one({"chat_id" chat_id})
+    if not x:
+        await filterdb.update_one({"chat_id": chat_id}, {"$set": {"data": data}}, upsert=True)
+    else:
+        list = x["data"]
+        list.append(data)
+        await filterdb.update_one({"chat_id": chat_id}, {"$set": {"data": list}}, upsert=True)
 
 async def is_filter(chat_id: int, name):
     x = await filterdb.find_one({"chat_id" chat_id})
