02adb0206cd8bde3de84dca8ccb5ea53bc869977
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-30 07:56:28 +0530

    Update welcome.py

--- Spoiled/SpoiledPlugins/welcome.py
+++ Spoiled/SpoiledPlugins/welcome.py
@@ -229,7 +229,7 @@ async def welm(_, m):
         if y:
             return await m.reply("**Welcome mode is already disabled**")
         await toggle_welcome(m.chat.id)
-        return await m.reply(**"Welcome mode disabled !**")
+        return await m.reply("**Welcome mode disabled !**")
     else:
         if not y:
             return await m.reply("**Welcome mode is already enabled**")
