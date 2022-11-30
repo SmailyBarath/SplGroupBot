3cab22cd074406b283d27edc1e380b4385ed3223
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-30 10:43:57 +0530

    Update fun.py

--- Spoiled/SpoiledPlugins/fun.py
+++ Spoiled/SpoiledPlugins/fun.py
@@ -11,12 +11,12 @@ from telegram.ext import CallbackContext
 GIF_ID = "CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE"
 
 
-def runs(update: Update, context: CallbackContext):
+async def runs(update: Update, context: CallbackContext):
     temp = random.choice(fun_strings.RUN_STRINGS)
-    update.effective_message.reply_text(temp)
+    await update.effective_message.reply_text(temp)
 
 
-def sanitize(update: Update, context: CallbackContext):
+async def sanitize(update: Update, context: CallbackContext):
     message = update.effective_message
     name = (
         message.reply_to_message.from_user.first_name
@@ -24,19 +24,19 @@ def sanitize(update: Update, context: CallbackContext):
         else message.from_user.first_name
     )
     reply_animation = (
-        message.reply_to_message.reply_animation
+        await message.reply_to_message.reply_animation
         if message.reply_to_message
-        else message.reply_animation
+        else await message.reply_animation
     )
     reply_animation(random.choice(fun_strings.GIFS), caption=f"*Sanitizes {name}*")
 
 
 
-def roll(update: Update, context: CallbackContext):
-    update.message.reply_text(random.choice(range(1, 7)))
+async def roll(update: Update, context: CallbackContext):
+    await update.effective_message.reply_text(random.choice(range(1, 7)))
 
 
-def shout(update: Update, context: CallbackContext):
+async def shout(update: Update, context: CallbackContext):
     args = context.args
     text = " ".join(args)
     result = []
@@ -47,32 +47,28 @@ def shout(update: Update, context: CallbackContext):
     result[0] = text[0]
     result = "".join(result)
     msg = "```\n" + result + "```"
-    return update.effective_message.reply_text(msg, parse_mode="MARKDOWN")
+    return await update.effective_message.reply_text(msg, parse_mode="MARKDOWN")
 
 
-def toss(update: Update, context: CallbackContext):
-    update.message.reply_text(random.choice(fun_strings.TOSS))
-
-
-def shrug(update: Update, context: CallbackContext):
+async def shrug(update: Update, context: CallbackContext):
     msg = update.effective_message
     reply_text = (
-        msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+        await msg.reply_to_message.reply_text if msg.reply_to_message else await msg.reply_text
     )
     reply_text(r"¯\_(ツ)_/¯")
 
 
-def bluetext(update: Update, context: CallbackContext):
+async def bluetext(update: Update, context: CallbackContext):
     msg = update.effective_message
     reply_text = (
-        msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+        await msg.reply_to_message.reply_text if msg.reply_to_message else await msg.reply_text
     )
     reply_text(
         "/BLUE /TEXT\n/MUST /CLICK\n/I /AM /A /STUPID /ANIMAL /THAT /IS /ATTRACTED /TO /COLORS",
     )
 
 
-def rlg(update: Update, context: CallbackContext):
+async def rlg(update: Update, context: CallbackContext):
     eyes = random.choice(fun_strings.EYES)
     mouth = random.choice(fun_strings.MOUTHS)
     ears = random.choice(fun_strings.EARS)
@@ -81,31 +77,45 @@ def rlg(update: Update, context: CallbackContext):
         repl = ears[0] + eyes[0] + mouth[0] + eyes[1] + ears[1]
     else:
         repl = ears[0] + eyes[0] + mouth[0] + eyes[0] + ears[1]
-    update.message.reply_text(repl)
+    await update.effective_message.reply_text(repl)
 
 
-def decide(update: Update, context: CallbackContext):
+async def decide(update: Update, context: CallbackContext):
     reply_text = (
-        update.effective_message.reply_to_message.reply_text
+        await update.effective_message.reply_to_message.reply_text
         if update.effective_message.reply_to_message
-        else update.effective_message.reply_text
+        else await update.effective_message.reply_text
     )
     reply_text(random.choice(fun_strings.DECIDE))
 
 
-def eightball(update: Update, context: CallbackContext):
+async def eightball(update: Update, context: CallbackContext):
     reply_text = (
-        update.effective_message.reply_to_message.reply_text
+        await update.effective_message.reply_to_message.reply_text
         if update.effective_message.reply_to_message
-        else update.effective_message.reply_text
+        else await update.effective_message.reply_text
     )
     reply_text(random.choice(fun_strings.EIGHTBALL))
 
 
-def table(update: Update, context: CallbackContext):
+async def table(update: Update, context: CallbackContext):
     reply_text = (
-        update.effective_message.reply_to_message.reply_text
+        await update.effective_message.reply_to_message.reply_text
         if update.effective_message.reply_to_message
-        else update.effective_message.reply_text
+        else await update.effective_message.reply_text
     )
     reply_text(random.choice(fun_strings.TABLE))
+
+
+Yashu.add_handler(CommandHandler("sanitize", sanitize))
+Yashu.add_handler(CommandHandler("run", runs))
+Yashu.add_handler(CommandHandler("pat", pat))
+Yashu.add_handler(CommandHandler("slap", slap))
+Yashu.add_handler(CommandHandler("roll", roll))
+Yashu.add_handler(CommandHandler("shrug", shrug))
+Yashu.add_handler(CommandHandler("rlg", rlg))
+Yashu.add_handler(CommandHandler("bluetext", bluetext))
+Yashu.add_handler(CommandHandler("decide", decide))
+Yashu.add_handler(CommandHandler("8ball", eightball))
+Yashu.add_handler(CommandHandler("shout", shout))
+Yashu.add_handler(CommandHandler("table", table))
