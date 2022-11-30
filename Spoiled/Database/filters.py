400a5d13d4f83fb02b3b8eec42a96465bf7181e5
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-30 16:43:55 +0530

    Update filters.py

--- Spoiled/Database/filters.py
+++ Spoiled/Database/filters.py
@@ -14,3 +14,34 @@ async def is_filter(chat_id: int, name):
         if c[0] == name:
             return True
     return False
+
+async def list_filters(chat_id: int):
+    x = await filterdb.find_one({"chat_id" chat_id})
+    if not x:
+        return []
+    list = x["data"]
+    lmao = []
+    for c in list:
+        lmao.append(c[0])
+    return lmao
+
+async def del_filter(chat_id: int, name):
+    x = await filterdb.find_one({"chat_id" chat_id})
+    if not x:
+        return
+    list = x["data"]
+    for c in list:
+        if c[0] == name:
+            list.remove(c)
+            return
+    return
+
+async def get_filter(chat_id: int, name):
+    x = await filterdb.find_one({"chat_id" chat_id})
+    if not x:
+        return {}
+    list = x["data"]
+    for c in list:
+        if c[0] == name:
+            return c[1]
+    return {}
