7e23326e05d47bc6e7336278837efd71eabe28d0
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-01 11:28:28 +0530

    Create start.py

---
+++ Strings/start.py
@@ -0,0 +1,16 @@
+from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
+from config import STUFF
+
+START_TEXT = """
+
+Hello {} !
+✪ I'm {}, an anime-themed group management bot ✨
+────────────────────────
+× Service Uptime: {}
+× {} users.
+────────────────────────
+✪ Hit /help for more info. [.](
+
+"""
+
+START_MARKUP = 
