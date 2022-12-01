72f29be2965cae10cb2e79c48f9f068c7cc9bdb7
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-01 11:49:00 +0530

    Update _start.py

--- Spoiled/SpoiledPlugins/_start.py
+++ Spoiled/SpoiledPlugins/_start.py
@@ -1,3 +1,15 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
+from String.start import START_TEXT, START_MARKUP
+from config import STUFF
+import time
+from . import startTime, botname
+from .afk import get_readable_time
+from Spoiled.Database.users import get_served_users
+from Spoiled.Database.pusers import add_served_puser
 
+@Client.on_message(filters.command("start") & filters.group)
+async def st(_, m):
+    fn = m.from_user.first_name
+    Upt = get_readable_time(int(time.time()-startTime))
+    x = await get_served_users
+    await m.reply_photo(STUFF.START_IMG, caption=START_TEXT.format(fn, botname, Upt, x), reply_markup=START_MARKUP)
