a41ec7da3e2cb4a721a8f87ab50de764868ffc22
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-03 17:11:44 +0530

    Update reverse.py

--- Spoiled/SpoiledPlugins/reverse.py
+++ Spoiled/SpoiledPlugins/reverse.py
@@ -3,17 +3,60 @@ from asyncio import gather, get_running_loop
 from base64 import b64decode
 from io import BytesIO
 from random import randint
-
+from aiohttp import ClientSession
 import aiofiles
 import requests
 from bs4 import BeautifulSoup
 from pyrogram import filters
 from pyrogram.types import InputMediaPhoto, Message
-
 from .chatbot import eor
-from nezuko.utils.functions import get_file_id_from_message
-from nezuko.utils.http import get
 
+session = ClientSession()
+async def get(url: str, *args, **kwargs):
+    async with session.get(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
+def get_file_id_from_message(
+    message,
+    max_file_size=3145728,
+    mime_types=["image/png", "image/jpeg"],
+):
+    file_id = None
+    if message.document:
+        if int(message.document.file_size) > max_file_size:
+            return
+
+        mime_type = message.document.mime_type
+
+        if mime_types and mime_type not in mime_types:
+            return
+        file_id = message.document.file_id
+
+    if message.sticker:
+        if message.sticker.is_animated:
+            if not message.sticker.thumbs:
+                return
+            file_id = message.sticker.thumbs[0].file_id
+        else:
+            file_id = message.sticker.file_id
+
+    if message.photo:
+        file_id = message.photo.file_id
+
+    if message.animation:
+        if not message.animation.thumbs:
+            return
+        file_id = message.animation.thumbs[0].file_id
+
+    if message.video:
+        if not message.video.thumbs:
+            return
+        file_id = message.video.thumbs[0].file_id
+    return file_id
 
 async def get_soup(url: str, headers):
     html = await get(url, headers=headers)
