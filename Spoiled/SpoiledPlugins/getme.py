3ac7f396fbfb175a163ea49977b0da3b463ba278
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-07 20:31:14 +0530

    Create getme.py

---
+++ Spoiled/SpoiledPlugins/getme.py
@@ -0,0 +1,6 @@
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("getme") & filters.group)
+async def geth(_, m):
+    x = await _.get_chat_member(m.chat.id, m.from_user.id)
+    await m.reply(x)
