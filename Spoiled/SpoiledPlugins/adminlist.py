afe5e5c2daeccb41661eb5fcfb364792e915625e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-07 23:17:52 +0530

    Create adminlist.py

---
+++ Spoiled/SpoiledPlugins/adminlist.py
@@ -0,0 +1,49 @@
+from pyrogram import Client, filters, enums
+
+creator = None
+admins = []
+bots = []
+deleted = []
+
+@Client.on_message(filters.command(["admins", "adminlist", "staff"]) & filters.group)
+async def al(_, m):
+    global creator, admins, bots, deleted
+    ok = await m.reply("**Fetching staff...**")
+    async for x in _.get_chat_members(m.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+        if x.user.is_bot:
+            bots.append(x.user.mention)
+        elif x.status.name == "OWNER":
+            creator = x.user.mention
+        elif x.user.is_deleted:
+            deleted.append(x.user.mention)
+        else:
+            admins.append(x.user.mention)
+
+    txt = f"**{m.chat.title} staff :**"
+    txt += "\n\n"
+    txt += " 👑**Creator :**"
+    txt += f" • {creator}"
+    txt += "\n\n"
+    if admins:
+        txt += " 👨‍💻**Admins :**"
+        for adm in admins:
+            txt += f" • {adm}"
+            txt += "\n"
+    txt += "\n\n"
+    if bots:
+        txt += " 🤖**Bots :**"
+        for adm in bots:
+            txt += f" • {adm}"
+            txt += "\n"
+    txt += "\n\n"
+    if deleted:
+        txt += " 👻**Admins :**"
+        for adm in deleted:
+            txt += f" • **None**"
+            txt += "\n"
+    try:
+        await ok.edit(txt)
+    except:
+        await m.reply(txt)
+    
+        
