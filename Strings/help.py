9298caf69b354ea9b05b9444374a0f897365791e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-09 13:00:35 +0530

    Update help.py

--- Strings/help.py
+++ Strings/help.py
@@ -43,3 +43,64 @@ Here is the help for the Anime ♓ module:
 
 """
 
+ANIMEQUOTES_HELP = """
+
+Here is the help for the AnimeQuotes🖍 module:
+
+
+ • /animequotes: gives random anime quotes
+
+"""
+
+ANTICHANNEL_HELP = """
+
+Here is the help for the Antichannel❌ module:
+
+Admin command :-
+
+❖ /antichannel (on/yes/off/no) - after enabling this it will ban and delete channel user mesaages. 
+ 
+ If u want to ban only a specific channel then reply that channel with /ban  and to unban it reply /unban
+
+"""
+
+APPROVAL_HELP = """
+
+Here is the help for the Approval✅ module:
+ 
+Sometimes, you might trust a user not to send unwanted content.
+Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
+That's what approvals are for - approve of trustworthy users to allow them to send
+Admin commands:
+❂ /approve: Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
+❂ /disapprove: Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
+❂ /approved: List all approved users.
+
+"""
+
+FLOOD_HELP = """
+
+Here is the help for the Anti-Flood🛡 module:
+
+Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood will result in restricting that user.
+
+ This will mute users if they send more than 10 messages in a row, bots are ignored.
+ • /flood: Get the current flood control setting
+
+• Admins only:
+ • /setflood <int/'no'/'off'>: enables or disables flood control
+ Example: /setflood 10
+ • /setfloodmode <ban/kick/mute/tban/tmute> <value>: Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+
+• Note:
+ • Value must be filled for tban and tmute!!
+ It can be:
+ 5m = 5 minutes
+ 6h = 6 hours
+ 3d = 3 days
+ 1w = 1 week
+
+"""
+
+
+
