20a02c3256ed1aa16c62e85127bab2ff692e44c5
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-09 12:56:21 +0530

    Update help.py

--- Strings/help.py
+++ Strings/help.py
@@ -1,1 +1,45 @@
+AFK_HELP = """
+
+Here is the help for the AFK💤 module:
+
+ • /afk <reason>: mark yourself as AFK(away from keyboard).
+ • brb <reason>: same as the afk command - but not a command.
+When marked as AFK, any mentions will be replied to with a message to say you're not available!
+
+"""
+
+ADMIN_HELP = """
+
+Here is the help for the Admins module:
+
+• /admins: list of admins in the chat
+Admins only:
+ • /pin: silently pins the message replied to - add 'loud' or 'notify' to give notifs to users
+ • /unpin: unpins the currently pinned message
+ • /invitelink: gets invitelink
+ • /promote: promotes the user replied to
+ • /fullpromote: Fully promotes the user replied to
+ • /demote: demotes the user replied to
+ • /title <title here>: sets a custom title for an admin that the bot promoted
+ • /admincache: force refresh the admins list
+ • /setgtitle <text>: set group title
+ • /setgpic: reply to an image to set as group photo
+ • /setdesc: Set group description
+
+"""
+
+ANIME_HELP = """
+
+Here is the help for the Anime ♓ module:
+
+ ◆/anime - Fetches info on single anime (includes
+          buttons to look up for prequels and
+          sequels)
+ ◆/character - Fetches info on multiple possible
+              characters related to query
+ ◆/manga - Fetches info on multiple possible
+          mangas related to query
+ ◆/airing - Fetches info on airing data for anime
+
+"""
 
