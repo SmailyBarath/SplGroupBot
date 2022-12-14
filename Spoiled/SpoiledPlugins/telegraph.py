6a45c69b3ee433cb57a0b0e03619a9bc4a9fdad7
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-14 14:15:28 +0530

    Create telegraph.py

---
+++ Spoiled/SpoiledPlugins/telegraph.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from telegraph import upload_file
+
+@Client.on_message(filters.command(["telegraph"]))
+async def get_link_private(client, message):
+    try:
+        text = await message.reply("Processing...")
+        async def progress(current, total):
+            await text.edit_text(f"📥 Downloading media... {current * 100 / total:.1f}%")
+        try:
+            location = f"./media/private/"
+            local_path = await message.download(location, progress=progress)
+            await text.edit_text("📤 Uploading to Telegraph...")
+            upload_path = upload_file(local_path) 
+            await text.edit_text(f"**🌐 | Telegraph Link**:\n\n<code>https://telegra.ph{upload_path[0]}</code>")     
+            os.remove(local_path) 
+        except Exception as e:
+            await text.edit_text(f"**❌ | File upload failed**\n\n<i>**Reason**: {e}</i>")
+            os.remove(local_path) 
+            return                 
+    except Exception:
+        pass        
