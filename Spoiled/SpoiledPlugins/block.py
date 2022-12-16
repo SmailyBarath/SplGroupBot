9b82021023b03853f82a63a03e3e1dbdf0bfad7b
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-16 22:26:42 +0530

    Create block.py

---
+++ Spoiled/SpoiledPlugins/block.py
@@ -0,0 +1,38 @@
+from config import DEV
+from Spoiled.Database.blocked import *
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+SUDOERS = DEV.SUDO_USERS + [DEV.OWNER_ID]
+
+@Client.on_message(filters.command(["block", "unblock"]) & ~filters.edited & ~filters.forwarded)
+async def bloblo(_, m: Message):
+    if m.from_user.id not in SUDOERS:
+        return
+    replied = m.reply_to_message
+    if replied:
+        a = m.reply_to_message.from_user.id
+    elif replied and len(m.command) > 1:
+        a = m.reply_to_message.from_user.id
+    elif (not replied and len(m.command) > 1):
+        xD = m.text.split()
+        try:
+            a = int(xD[1])
+        except:
+            await m.reply("please enter id only")
+    else:
+        await m.reply("Try /block | /unblock [ reply | id ]")
+    if a in SUDOERS:
+        return await m.reply("you can't block the owner of this bot !")
+
+    if m.text.split()[0][1].lower() == "u":
+        blocked = await is_blocked(a)
+        if blocked:
+            await unblock(a)
+            await m.reply("user unblocked")
+    else:
+        blocked = await is_blocked(a)
+        if not blocked:
+            await block(a)
+            await m.reply("user blocked")
+    
