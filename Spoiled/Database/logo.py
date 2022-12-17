dc752358e2373fa53345093fda7e7613736b93ca
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 20:52:36 +0530

    Create logo.py

---
+++ Spoiled/Database/logo.py
@@ -0,0 +1,15 @@
+from . import db
+
+logodb = db.logo
+
+async def get_latest_id(chat_id):
+    x = logodb.find({"chat_id": chat_id})
+    if x:
+    c = int(x["id"])
+        return c
+
+async def set_latest_id(chat_id, id):
+    x = logodb.find({"chat_id": chat_id})
+    if x:
+        logodb.delete_one({"chat_id": chat_id})
+    logodb.update_one({"chat_id": chat_id}, {"$set": {"id": id}}, upsert=True)
