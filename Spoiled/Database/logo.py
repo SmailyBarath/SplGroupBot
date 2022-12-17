2c83bfa06d95a07184e7695b1292042264ae5b22
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 23:43:51 +0530

    Update logo.py

--- Spoiled/Database/logo.py
+++ Spoiled/Database/logo.py
@@ -9,7 +9,7 @@ async def get_latest_id(chat_id):
         return c
 
 async def set_latest_id(chat_id, id):
-    x = logodb.find({"chat_id": chat_id})
+    x = await logodb.find_one({"chat_id": chat_id})
     if x:
-        logodb.delete_one({"chat_id": chat_id})
-    logodb.update_one({"chat_id": chat_id}, {"$set": {"id": id}}, upsert=True)
+        await logodb.delete_one({"chat_id": chat_id})
+    await logodb.update_one({"chat_id": chat_id}, {"$set": {"id": id}}, upsert=True)
