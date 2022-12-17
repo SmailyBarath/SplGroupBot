cffd9923028d8bf077f3a0e4b001bd514f41f68d
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-17 23:42:52 +0530

    Update logo.py

--- Spoiled/SpoiledPlugins/logo.py
+++ Spoiled/SpoiledPlugins/logo.py
@@ -6,11 +6,12 @@ import random
 import re
 import glob
 import time
-from Spoiled.Database.logo import get_latest_id
+from Spoiled.Database.logo import get_latest_id, set_latest_id
 from io import BytesIO
 from requests import get
 from pyrogram import Client, filters
 from config import CHATS
+from config import DEV
 from PIL import Image, ImageDraw, ImageFont
 
 
