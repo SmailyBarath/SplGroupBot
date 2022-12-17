8a1da3ce258b1648b70b33eb8e8579405cb6ea4a
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-17 12:07:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv as e
 
 class API:
-    API_ID = e("API_ID", None)
-    API_HASH = e("API_HASH", None)
+    API_ID = e("API_ID", 10763476)
+    API_HASH = e("API_HASH", "e7d6d5493a896264a09d04fda7a30f9d")
 
 class TOKENS:
-    BOT_TOKEN = e("BOT_TOKEN", None)
+    BOT_TOKEN = e("BOT_TOKEN", "5878056492:AAGCL9aAQPIqmDhkLWZf3bOYt8UdmS8qe7Q")
     ARQ_API_KEY = e("ARQ_API_KEY", "PNZJLN-ZZFHVK-USQLIZ-MQEWJN-ARQ")
 
 class DEV:
@@ -14,7 +14,7 @@ class DEV:
     OWNER_USERNAME = e("OWNER_USERNAME", "ShutupKeshav")
 
 class DATABASE:
-    MONGO_DB_URL = e("MONGO_DB_URL", None)
+    MONGO_DB_URL = e("MONGO_DB_URL", "mongodb+srv://musicbot:<password>@cluster0.61lydz4.mongodb.net/?retryWrites=true&w=majority")
     SQL_DB_URL = e("SQL_DB_URL", None)
 
 class STUFF:
@@ -23,4 +23,4 @@ class STUFF:
 
 class CHATS:
     SUPPORT_CHAT = e("SUPPORT_CHAT", "Spoiled_Community")
-    LOG_GROUP_ID = int(e("LOG_GROUP_ID", None))
+    LOG_GROUP_ID = int(e("LOG_GROUP_ID", -1001643158613))
