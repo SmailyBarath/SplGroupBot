811b79464a70ffabd6017615e8399c5b293c365a
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-17 12:45:48 +0530

    Update warn.py

--- Spoiled/Database/warn.py
+++ Spoiled/Database/warn.py
@@ -14,3 +14,24 @@ async def warn_user(chat_id: int, user_id: int):
     else:
         warns = {"warns": {user_id: 1}}
         return await warndb.update_one({"chat_id": chat_id}, {"$set": warns}, upsert=True)
+
+async def dwarn_user(chat_id: int, user_id: int):
+    x = await warndb.find_one({"chat_id": chat_id})
+    if not x:
+        return
+    warns = x["warns"]
+    if not user_id in warns:
+        return
+    h = warns[user_id]
+    h -= 1
+    return await warndb.update_one({"chat_id", chat_id}, {"set": {"warns": {user_id: h}}}, upsert=True)
+
+async def get_warns(chat_id: int, user_id: int):
+    x = await warndb.find_one({"chat_id": chat_id})
+    if not x:
+        return 0
+    warns = x["warns"]
+    if not user_id in warns:
+        return 0
+    h = warns[user_id]
+    return h
