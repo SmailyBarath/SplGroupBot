cbc265744f72d9ed1ef481e89a2e762226d31da6
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-18 18:45:19 +0530

    Update inlinehelp.py

--- Spoiled/SpoiledPlugins/inlinehelp.py
+++ Spoiled/SpoiledPlugins/inlinehelp.py
@@ -1,1 +1,17 @@
 from .chatbot import aiohttpsession as session
+
+async def post(url: str, *args, **kwargs):
+    async with session.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
+BASE = "https://batbin.me/"
+
+async def paste(content: str):
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
+        return
+    return BASE + resp["message"]
