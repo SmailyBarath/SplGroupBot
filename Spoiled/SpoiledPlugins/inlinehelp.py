01189184f29a17d08dd363cc1929a7aa75788c65
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-18 18:50:39 +0530

    Update inlinehelp.py

--- Spoiled/SpoiledPlugins/inlinehelp.py
+++ Spoiled/SpoiledPlugins/inlinehelp.py
@@ -1,5 +1,37 @@
 from .chatbot import aiohttpsession as session
 import speedtest
+import datetime
+from pytube import YouTube
+
+def download_youtube_audio(arq_resp):
+    r = arq_resp.result[0]
+
+    title = r.title
+    performer = r.channel
+
+    m, s = r.duration.split(":")
+    duration = int(
+        datetime.timedelta(minutes=int(m), seconds=int(s)).total_seconds()
+    )
+
+    if duration > 1800:
+        return
+
+    thumb = get(r.thumbnails[0]).content
+    with open("thumbnail.png", "wb") as f:
+        f.write(thumb)
+    thumbnail_file = "thumbnail.png"
+
+    url = f"https://youtube.com{r.url_suffix}"
+    yt = YouTube(url)
+    audio = yt.streams.filter(only_audio=True).get_audio_only()
+
+    out_file = audio.download()
+    base, _ = os.path.splitext(out_file)
+    audio_file = base + ".mp3"
+    os.rename(out_file, audio_file)
+
+    return [title, performer, duration, audio_file, thumbnail_file]
 
 async def post(url: str, *args, **kwargs):
     async with session.post(url, *args, **kwargs) as resp:
