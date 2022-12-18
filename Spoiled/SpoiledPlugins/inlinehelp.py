973e3c42b6cf4c850ae9fa72e0301834c6891eae
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-18 18:47:58 +0530

    Update inlinehelp.py

--- Spoiled/SpoiledPlugins/inlinehelp.py
+++ Spoiled/SpoiledPlugins/inlinehelp.py
@@ -1,4 +1,5 @@
 from .chatbot import aiohttpsession as session
+import speedtest
 
 async def post(url: str, *args, **kwargs):
     async with session.post(url, *args, **kwargs) as resp:
@@ -15,3 +16,19 @@ async def paste(content: str):
     if not resp["success"]:
         return
     return BASE + resp["message"]
+
+def test_speedtest():
+    def speed_convert(size):
+        power = 2 ** 10
+        zero = 0
+        units = {0: "", 1: "Kb/s", 2: "Mb/s", 3: "Gb/s", 4: "Tb/s"}
+        while size > power:
+            size /= power
+            zero += 1
+        return f"{round(size, 2)} {units[zero]}"
+
+    speed = speedtest.Speedtest()
+    info = speed.get_best_server()
+    download = speed.download()
+    upload = speed.upload()
+    return [speed_convert(download), speed_convert(upload), info]
