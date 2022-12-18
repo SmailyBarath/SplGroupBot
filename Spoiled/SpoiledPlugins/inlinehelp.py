1cd84ce23588fb56f7a453aff9c4456211fd306c
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-18 18:57:45 +0530

    Update inlinehelp.py

--- Spoiled/SpoiledPlugins/inlinehelp.py
+++ Spoiled/SpoiledPlugins/inlinehelp.py
@@ -3,6 +3,68 @@ import speedtest
 import datetime
 from pytube import YouTube
 from yashu import spoil as app
+from typing import List, Optional, Union
+import pyrogram
+from pyrogram import raw, types, utils
+from pyrogram.file_id import DOCUMENT_TYPES, PHOTO_TYPES, FileId, FileType
+from pyrogram.types import InlineQueryResult
+
+class InlineQueryResultCachedDocument(InlineQueryResult):
+    def __init__(
+            self,
+            file_id: str,
+            title: str = None,
+            id: str = None,
+            description: str = None,
+            caption: str = "",
+            parse_mode: Optional[str] = object,
+            caption_entities: List["types.MessageEntity"] = None,
+            reply_markup: "types.InlineKeyboardMarkup" = None,
+            input_message_content: "types.InputMessageContent" = None,
+    ):
+        super().__init__("file", id, input_message_content, reply_markup)
+
+        self.file_id = file_id
+        self.title = title
+        self.description = description
+        self.caption = caption
+        self.caption_entities = caption_entities
+        self.parse_mode = parse_mode
+        self.reply_markup = reply_markup
+        self.input_message_content = input_message_content
+
+    async def write(self, client: "pyrogram.Client"):
+        document = get_input_file_from_file_id(self.file_id)
+
+        message, entities = (
+            await utils.parse_text_entities(
+                client,
+                self.caption,
+                self.parse_mode,
+                self.caption_entities,
+            )
+        ).values()
+
+        return raw.types.InputBotInlineResultDocument(
+            id=self.id,
+            type=self.type,
+            title=self.title,
+            description=self.description,
+            document=document,
+            send_message=(
+                await self.input_message_content.write(
+                    client, self.reply_markup
+                )
+                if self.input_message_content
+                else raw.types.InputBotInlineMessageMediaAuto(
+                    reply_markup=await self.reply_markup.write(client)
+                    if self.reply_markup
+                    else None,
+                    message=message,
+                    entities=entities,
+                )
+            ),
+        )
 
 def download_youtube_audio(arq_resp):
     r = arq_resp.result[0]
