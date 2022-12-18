c397af4de2165003762efd28452cf9c3d2651031
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-18 18:44:51 +0530

    Update inlinefunc.py

--- Spoiled/SpoiledPlugins/inlinefunc.py
+++ Spoiled/SpoiledPlugins/inlinefunc.py
@@ -40,22 +40,6 @@ from wbb.modules.info import get_chat_info, get_user_info
 from wbb.modules.music import download_youtube_audio
 from wbb.utils.functions import test_speedtest
 
-async def post(url: str, *args, **kwargs):
-    async with session.post(url, *args, **kwargs) as resp:
-        try:
-            data = await resp.json()
-        except Exception:
-            data = await resp.text()
-    return data
-
-BASE = "https://batbin.me/"
-
-async def paste(content: str):
-    resp = await post(f"{BASE}api/v2/paste", data=content)
-    if not resp["success"]:
-        return
-    return BASE + resp["message"]
-
 keywords_list = [
     "image",
     "wall",
