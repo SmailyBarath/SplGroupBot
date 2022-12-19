e761dd605470eb626ace2ffc2a370b3d60d1b805
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-19 18:26:59 +0530

    Create about.py

---
+++ Strings/about.py
@@ -0,0 +1,21 @@
+ABOUT_TEXT = """
+
+**About {}**
+
+• Made with pyrogram
+
+• Not clone of anime bots
+
+• Used mongo as Database
+
+• Hosting on private server
+
+• No sudo and etc issues
+
+• Full privacy and safe
+
+• Owner - @ShutupKeshav (self coded)
+
+• Soon you'll get to see more, @SpLBots
+
+"""
