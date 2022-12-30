10a2324ff8dcb21f035ac611f2fbda6a10a02fd2
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-30 18:15:28 +0530

    Create hugs.py

---
+++ Spoiled/Database/hugs.py
@@ -0,0 +1,15 @@
+from . import db
+
+logodb = db.hugs
+
+async def get_latest_id(chat_id):
+    x = await logodb.find_one({"chat_id": chat_id})
+    if x:
+        c = int(x["id"])
+        return c
+
+async def set_latest_id(chat_id, id):
+    x = await logodb.find_one({"chat_id": chat_id})
+    if x:
+        await logodb.delete_one({"chat_id": chat_id})
+    await logodb.update_one({"chat_id": chat_id}, {"$set": {"id": id}}, upsert=True)
