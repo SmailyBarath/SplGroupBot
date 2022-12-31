6723f9dcee610481d64c2f41c8fcae1df2e4a13d
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-31 17:31:18 +0530

    Update etc.py

--- Spoiled/SpoiledPlugins/etc.py
+++ Spoiled/SpoiledPlugins/etc.py
@@ -36,6 +36,40 @@ async def hugger(_, m):
     else:
         return await m.reply_video(x.video.file_id, caption=cap if cap else "")
 
+@Client.on_message(filters.command("bite"))
+async def hugger(_, m):
+    if m.reply_to_message:
+        men1 = m.from_user.mention
+        men2 = m.reply_to_message.from_user.mention
+        cap = f"{men1} biting {men2} 💥 !"
+    else:
+        if len(m.command) > 1:
+            men1 = m.from_user.mention
+            txt = m.text.split()[1]
+            try:
+                men2 = (await _.get_users(txt)).mention
+                cap = f"{men1} biting {men2} 💥 !"
+            except:    
+                cap = None
+        else:
+            cap = None
+
+    id = await get_latest_id(-1001605629866)
+    num = []
+    for i in range(1, id+1):
+        num.append(i)
+    mid = random.choice(num)
+    x = await _.get_messages(-1001605629866, mid)
+    while (not x.photo and not x.video and not x.animation) or x.caption != "#bite":
+        mid = random.choice(num)
+        x = await _.get_messages(-1001605629866, mid)
+    if x.photo:
+        return await m.reply_photo(x.photo.file_id, caption=cap if cap else "")
+    elif x.animation:
+        return await m.reply_animation(x.animation.file_id, caption=cap if cap else "")
+    else:
+        return await m.reply_video(x.video.file_id, caption=cap if cap else "")
+
 @Client.on_message(filters.command("curse"))
 async def curser(_, m):
     if m.reply_to_message:
